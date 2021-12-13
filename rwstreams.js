7e36a6cdbde6b8078a75fc233551152ef0b49de6
jonniek01 <jontejonhn@gmail.com>
2021-12-13 05:42:53 +0300

    r-w-streams

--- rwstreams.js
+++ rwstreams.js
@@ -1,1 +1,25 @@
 const fs=require("fs");
+
+const readStream=fs.createReadStream("./file0.txt","utf-8");
+const readStream2=fs.createReadStream("./large.txt","utf-8");
+
+const writeStream=fs.createWriteStream("./file1.txt");
+
+readStream.on("data",(chunk)=>{
+    writeStream.write(chunk);
+});
+
+
+/*readFile=fs.readFile("large.txt","utf-8",(err,file)=>{
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log(file)
+    }
+});
+*/
+
+readStream2.on("data",(chunk)=>{
+    console.log(chunk);
+});
