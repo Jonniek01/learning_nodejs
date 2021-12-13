2fd455877db7380fda8fe1fed0d298bf5a9d90a7
jonniek01 <jontejonhn@gmail.com>
2021-12-13 06:07:49 +0300

    p and p chaining

---
+++ ppchaining.js
@@ -0,0 +1,17 @@
+const fs=require("fs");
+const zlib=require("zlib");
+const gzip=zlib.createGzip();
+
+const readStream=fs.createReadStream("./file0.txt","utf-8");
+
+const writeStream=fs.createWriteStream("./file1.txt.gz");
+
+readStream.pipe(gzip).pipe(writeStream);
+
+// const gunzip=zlib.createGunzip();
+
+// const readStream1=fs.createReadStream("./file1.txt.gz");
+
+// const writeStream1=fs.createWriteStream("./file2.txt");
+
+// readStream1.pipe(gunzip).pipe(writeStream1);
