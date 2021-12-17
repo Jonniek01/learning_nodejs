52550f74081b0a52c502ca7e6ba3f7bd0d0225c0
jonniek01 <jontejonhn@gmail.com>
2021-12-17 05:23:02 +0300

    serving static files, package json, npm init,npm i

---
+++ servingstaticfiles.js
@@ -0,0 +1,7 @@
+const fs=require("fs");
+const http=require("http");
+http.createServer((req,res)=>{
+    const readStream=fs.createReadStream("./static/example.json");
+    res.writeHead(200,{"content-type":"html"});
+    readStream.pipe(res);
+}).listen(3000);
